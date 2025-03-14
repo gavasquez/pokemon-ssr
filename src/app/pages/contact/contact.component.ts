d71d702d240df0a9268c838ea397e59afeda143e
gavasquez <gustavovasquez15@gmail.com>
2025-03-14 15:36:18 -0500

    Fin de la seccion 2

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-contact',
@@ -7,4 +13,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './contact.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ContactComponent {}
+export default class ContactComponent implements OnInit {
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('Contact Page');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Este es mi contact page',
+    });
+    this.meta.updateTag({ name: 'og:title', content: 'Contact Page' });
+    this.meta.updateTag({
+      name: 'keywords',
+      content: 'Hola,Mundo,Gustavo,Vasquez,Curso,Angular',
+    });
+  }
+}
